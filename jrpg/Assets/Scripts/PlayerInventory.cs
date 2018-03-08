586598d1d3d42daceb68e570c11e3272688f270c
btbertram <brennantbertram@gmail.com>
2018-03-08 16:35:28 +0000

    Player Inventory
    Base for Player Inventory is now finished. Will add more functionality
    based on need/item types added.

--- jrpg/Assets/Scripts/PlayerInventory.cs
+++ jrpg/Assets/Scripts/PlayerInventory.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : ScriptableObject {
+public class Inventory : MonoBehaviour {
 
     //Player inventory is created once and goes though scene changes.
     //What should we do for other inventories? Random generation? Will they be saved?
     //Temp idea: Generate shop inventories based on level/rep/other factors. They are not saved, "Stock" is destroyed after the player leaves.
     //However, shops can only be accessed on a timer.(?)
-    public static Inventory m_Inventory;
-    //public Inventory m_inventory;
+    public static Inventory m_Player_Inventory;
+    public Inventory m_inventory;
+
+    public static bool isPlayerInven;
 
     //Enum for safer access of inventory elements. e_defualt should always remain as the first entry for error handling reasons (since that makes it 0).
     public enum InvenKeys {
@@ -37,7 +39,7 @@ public class Inventory : ScriptableObject {
     int inventoryType;
 
     //temporary forward declarations
-    class Item { };
+    class Item { public string name; };
     class Ship : Item { };
     class Gun : Item { };
     class Perk : Item { };
@@ -46,7 +48,7 @@ public class Inventory : ScriptableObject {
     IList<Gun> gunInven = new List<Gun>();
     IList<Perk> perkInven = new List<Perk>();
 
-    List<int> errorList = new List<int>();
+    //List<int> errorList = new List<int>();
 
     //Sets the current item type/type to be worked with.
     //Accesses the InvenRef Dictionary with the provided string to give an associated item type number.
@@ -64,7 +66,33 @@ public class Inventory : ScriptableObject {
         }
     }
 
-    void deleteInvenItem(int inventoryType, int selectedItemIndex)
+
+
+    {
+        switch (inventoryType)
+        {
+            case (int)InvenKeys.e_perk:
+                {
+                }
+                return listout;
+                //break;
+            case (int)InvenKeys.e_ship:
+                {
+                }
+                return listout;
+                //break;
+            case (int)InvenKeys.e_gun:
+                {
+                }
+                return listout;
+                //break;
+            default:
+                return listout;
+                //break;
+        }
+    }
+
+    void DeleteInvenItem(int selectedItemIndex)
     {
         switch (inventoryType)
         {
@@ -104,46 +132,29 @@ public class Inventory : ScriptableObject {
 
     }
 
-    //Ask about inheretance. Will need to create seperate functions otherwise...
-    void AddItem(int inventoryType, Item other)
+    void AddItem(Perk other)
     {
-        switch (inventoryType)
+        if (inventoryType == (int)InvenKeys.e_perk)
         {
-            case (int)InvenKeys.e_perk:
-                if (perkInven[selectedItemIndex] != null)
-                {
-                    perkInven.Add(other);
-                }
-                else
-                {
-                    Debug.Log("Error. Perk Inventory not found.");
-                }
-                break;
-            case (int)InvenKeys.e_ship:
-                if (shipInven[selectedItemIndex] != null)
-                {
-                    shipInven.RemoveAt(selectedItemIndex);
-                }
-                else
-                {
-                    Debug.Log("Error. Ship Inventory not found.");
-                }
-                break;
-            case (int)InvenKeys.e_gun:
-                if (gunInven[selectedItemIndex] != null)
-                {
-                    gunInven.RemoveAt(selectedItemIndex);
-                }
-                else
-                {
-                    Debug.Log("Error. Gun Inventory not found.");
-                }
-                break;
-            default:
-                break;
+            perkInven.Add(other);
         }
     }
 
+    void AddItem(Ship other)
+    {
+        if (inventoryType == (int)InvenKeys.e_ship)
+        {
+            shipInven.Add(other);
+        }
+    }
+
+    void AddItem(Gun other)
+    {
+        if (inventoryType == (int)InvenKeys.e_gun)
+        {
+            gunInven.Add(other);
+        }
+    }
 
 
     //Start section: Tutor examples given
@@ -214,9 +225,9 @@ public class Inventory : ScriptableObject {
     void Awake()
     {
         //Creates a singleton object. This Object will exsist between unity scene loads.
-        if (m_Inventory == null)
+        if (m_Player_Inventory == null && isPlayerInven)
         {
-            m_Inventory = this;
+            m_Player_Inventory = this;
         }
         DontDestroyOnLoad(this);
 
@@ -231,7 +242,7 @@ public class Inventory : ScriptableObject {
     void Start () {
 	
 	}
-	
+	    
 	// Update is called once per frame
 	void Update () {
 		
