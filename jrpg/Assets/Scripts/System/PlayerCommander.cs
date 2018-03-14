55e12222e39e54fafbb2a65ce0cafa88e8b78b9d
btbertram <brennantbertram@gmail.com>
2018-03-14 15:45:15 +0000

    New Branch, attempting to merge
    Cleaned up some file structure (two of our folders were named "Assets"
    so the sprites folder was moved out of the secondary assets folder and
    moved into the main assets folder) and added my scripts for the
    Inventory system, pre-battle system, as well as the base for the Game
    State system and the player/commander object. These are from the
    Inventory-PreBattle branch I orginally created, but was unable to merge
    due to conflicts.
    
    This new branch will also be a test to see if the master branch has
    become damaged.

---
+++ jrpg/Assets/Scripts/System/PlayerCommander.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommander : MonoBehaviour {
+
+    //naming conventions: Variables are camalCase. Functions CaptializeEachWord.
+    //Get: Returns a VALUE, used for caculations in other functions/formulas. Show: Returns a STRING, used for showing information to the player via UI.
+
+    //Level is based of an exp table- consider it a shorthand for total exp.
+    public static int level = 1;
+    int exp = 0;
+    //This would also determine any species perks. Species does not change.
+    private string species;
+    public string flaw;
+    //Power of perk increases with commander level. Perk Slots increase from 1 to 3 based on level.
+    public string currentPerk1;
+    public string currentPerk2;
+    public string currentPerk3;
+    //Morale can go from 0-100 (temporary). Determines fleet loyalty(flee chance) and perk strength (?)
+    int fleetMorale = 50;
+    //Rep can go from 0 to 100/1,000/10,000 (temporary). Determines shop availabilities, mission availabilities.
+    //Losing battles drops it, winning battles raises it, cap increases per area unlocked(?).
+    //Battles in further areas give more rep.
+    int reputation = 25;
+    int repCap = 100;
+    //starting cash in "nuts" (temporary)
+    int money = 1000;
+
+    //Inventory slot, item name (?)
+    IDictionary<int, string> playerInventory = new Dictionary<int, string>();
+
+    //Would likely be used to help display status on menu
+    void ShowLevel() { //have this stringstreamed, return a string to display //return level;
+    }
+    public static int GetLevel()
+    {
+        return level;
+    }
+    void GetExp() { }
+    //and in shops
+    void GetMoney() { }
+    void GetRep() { }
+    //Should we make this display as vague value?
+    void GetMorale() { }
+    void GetRepCap() { }
+    //This would likely be attached to a menu/UI button
+    void SetPerk() { }
+    void GetPerks() { }
+    void GetFlaw() { }
+
+    int Flaw
+    {
+        get
+        {
+            return reputation * level;
+        }
+
+        set
+        {
+            reputation = value;
+        }
+    }
+
+
+    //setters
+    void ChangePerk() { }
+
+    //exp thresholds are placeholders
+    //switchcase for level gain
+    /*Total up exp and gained exp, then drop current level into a switch. If exp triggers a new threshold, sets a new level and plays a level up animation (not yet implemented. Beta feature?)*/
+    void AddExp(int expGained) {
+        exp += expGained;
+        switch (level)
+        {
+            case 1:
+                if(exp > 1000)
+                {
+                    //PlayLevelUpAnim();
+                    level = 2;
+                }
+                break;
+            case 2:
+                if (exp > 4000)
+                {
+                    //PlayLevelUpAnim();
+                    level = 3;
+                }
+                break;
+            case 3:
+                if (exp > 10000)
+                {
+                    //PlayLevelUpAnim();
+                    level = 4;
+                }
+                break;
+            case 4:
+                if (exp > 22000)
+                {
+                    //PlayLevelUpAnim();
+                    level = 5;
+                }
+                break;
+            case 5:
+                if (exp > 46000)
+                {
+                    //PlayLevelUpAnim();
+                    level = 6;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void AlterRep(int repDifference)
+    {
+        reputation += repDifference;
+        Flaw = 5;
+        if (Flaw == 25)
+        {
+
+        }
+    }
+    void AlterMorale(int moraleDifference)
+    {
+        fleetMorale += moraleDifference;
+    }
+
+    void AlterMoney(int moneyDifference)
+    {
+        money += moneyDifference;
+    }
+
+    void OnBattleVictory(int expGained, int moneyGained, int repGained)
+    {
+        AddExp(expGained);
+        AlterMoney(moneyGained);
+        AlterRep(repGained);
+    }
+
+    void OnBattleLoss(int repLost)
+    {
+        AlterRep(repLost);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ jrpg/Assets/Scripts/System/PlayerInventory.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    //Player inventory is created once and goes though scene changes.
+    //What should we do for other inventories? Random generation? Will they be saved?
+    //Temp idea: Generate shop inventories based on level/rep/other factors. They are not saved, "Stock" is destroyed after the player leaves.
+    //However, shops can only be accessed on a timer.(?)
+
+    //To use: call SetInven("Inventory name"), then call DisplayInven().
+    //Use the index of the wanted item's string in the returned list with other inventory functions. 
+
+    public static Inventory m_Player_Inventory;
+    public Inventory m_inventory;
+
+    public bool isPlayerInven;
+
+    //Enum for safer access of inventory elements. e_defualt should always remain as the first entry for error handling reasons (since that makes it 0).
+    public enum InvenKeys {
+        e_default,
+        e_perk,
+        e_ship,
+        e_gun,
+        e_module,
+        e_recovery,
+        e_material
+    };
+    //Use last element in enum InvenKeys to obtain the total indexes (if we need it)
+    int totalIndexInvenKeys = (int)InvenKeys.e_material;
+
+    //This following dictionary is intended to allow users to access the inventories by name (string)
+    //To elaborate, we allow the user to give a string to the inventory, which converts it to an int via the dictionary.
+    //The enum InvenKeys helps for readability and to keep those ints constant.
+    /*
+    CURRENT RELEVENT NAMES/STRINGS ARE AS FOLLOWS:
+    "Perks"
+    "Guns"
+    "Ships"
+    */
+    IDictionary<string, int> InvenRef = new Dictionary<string, int>();
+    int inventoryType;
+
+    //temporary forward declarations
+    class Item { public string name; };
+    class Ship : Item { };
+    class Gun : Item { };
+    class Perk : Item { };
+
+    IList<Ship> shipInven = new List<Ship>();
+    IList<Gun> gunInven = new List<Gun>();
+    IList<Perk> perkInven = new List<Perk>();
+
+    //List<int> errorList = new List<int>();
+
+    //Sets the current item type/type to be worked with.
+    //Accesses the InvenRef Dictionary with the provided string to give an associated item type number.
+    void SetInven(string itemTypeName)
+    {
+        int inventoryTypeIndex;
+        if (InvenRef.TryGetValue(itemTypeName, out inventoryTypeIndex))
+        {
+            inventoryType = inventoryTypeIndex;
+        }
+        else
+        {
+            Debug.Log("Error. Requested inventory type not found.");
+            inventoryType = (int)InvenKeys.e_default;
+        }
+    }
+
+    //Returns a list of strings, each index holding the name of an item.
+    //This index should match the index location of the list the item is stored in.
+    //You can use that index to access that specific item.
+    List<string> DisplayInven()
+    {
+        List<string> listout = new List<string>();
+        switch (inventoryType)
+        {
+            case (int)InvenKeys.e_perk:
+                for (int i =0; i<perkInven.Count; i++)
+                {
+                    listout.Add(perkInven[i].name);
+                }
+                return listout;
+                //break;
+            case (int)InvenKeys.e_ship:
+                for (int i = 0; i < perkInven.Count; i++)
+                {
+                    listout.Add(shipInven[i].name);
+                }
+                return listout;
+                //break;
+            case (int)InvenKeys.e_gun:
+                for (int i = 0; i < gunInven.Count; i++)
+                {
+                    listout.Add(gunInven[i].name);
+                }
+                return listout;
+                //break;
+            default:
+                listout.Add("Error displaying Inventory.");
+                return listout;
+                //break;
+        }
+    }
+
+    //Directly returns the item in question.
+    //Unsure if a copy or pointer is returned. As such, unsure if modifications to the returned item will affect the item in the inventory.
+    //TESTING REQUIRED.
+    //Seperate functions required because of c# strong typing.
+    Perk AccessPerk(int selectedItemIndex)
+    {
+        return perkInven[selectedItemIndex];
+    }
+    Ship AccessShip(int selectedItemIndex)
+    {
+        return shipInven[selectedItemIndex];
+    }
+    Gun AccessGun(int selectedItemIndex)
+    {
+        return gunInven[selectedItemIndex];
+    }
+
+    //Overloaded fuctions to add items to their specific inventories.
+    //Could be improved via inheritance and interfaces? Ask tutors
+    void AddItem(Perk other)
+    {
+        if (inventoryType == (int)InvenKeys.e_perk)
+        {
+            perkInven.Add(other);
+        }
+    }
+
+    void AddItem(Ship other)
+    {
+        if (inventoryType == (int)InvenKeys.e_ship)
+        {
+            shipInven.Add(other);
+        }
+    }
+
+    void AddItem(Gun other)
+    {
+        if (inventoryType == (int)InvenKeys.e_gun)
+        {
+            gunInven.Add(other);
+        }
+    }
+
+
+    //Use the index from DisplayInven() to make sure the correct item is selected.
+    void DeleteInvenItem(int selectedItemIndex)
+    {
+        switch (inventoryType)
+        {
+            case (int)InvenKeys.e_perk:
+                if (perkInven[selectedItemIndex] != null)
+                {
+                    perkInven.RemoveAt(selectedItemIndex);
+                }
+                else
+                {
+                    Debug.Log("Error. Perk Inventory not found.");
+                }
+                break;
+            case (int)InvenKeys.e_ship:
+                if (shipInven[selectedItemIndex] != null)
+                {
+                    shipInven.RemoveAt(selectedItemIndex);
+                }
+                else
+                {
+                    Debug.Log("Error. Ship Inventory not found.");
+                }
+                break;
+            case (int)InvenKeys.e_gun:
+                if (gunInven[selectedItemIndex] != null)
+                {
+                    gunInven.RemoveAt(selectedItemIndex);
+                }
+                else
+                {
+                    Debug.Log("Error. Gun Inventory not found.");
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //Start section: Tutor examples given
+    //bool LoadData
+    //{
+    //    get
+    //    {
+    //        return loaddata;
+    //    }
+    //    set
+    //    {
+
+    //    }
+    //}
+
+    ////public class ItemType
+    ////{
+    ////}
+
+    //public interface IItem
+
+
+    //{
+    //    int Item
+    //    {
+    //        get;
+    //        set;
+    //    }
+    //}
+
+    //public string[] AllItems
+    //    {
+    //        get
+    //        {
+    //            for 
+    //        }
+    //    }
+
+    //bool toggle_thingy;
+    //public int Thing
+    //{
+    //    get
+    //    {
+    //        if (toggle_thingy)
+    //        {
+
+    //        }
+    //        else
+    //        {
+
+    //        }
+
+    //        return ((toggle_thingy) ? 5: 4);
+    //    }
+
+    //    set
+    //    {
+    //        toggle_thingy = (value % 1 == 0) ? true : false;
+
+    //    }
+    //}
+
+    //End Section: Tutor examples given
+
+    //Pass a casted enum of InvenKeys into this.
+
+
+    void Awake()
+    {
+        //Creates a singleton object. This Object will exsist between unity scene loads.
+        //Note: Set this as the player inventory/set isPlayerInven in Unity's UI.
+        if (m_Player_Inventory == null && isPlayerInven)
+        {
+            m_Player_Inventory = this;
+        }
+        DontDestroyOnLoad(this);
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        //This populates the inventory reference database. Don't touch.
+        InvenRef.Add(new KeyValuePair<string, int>("Default", (int)InvenKeys.e_default));
+        InvenRef.Add(new KeyValuePair<string, int>("Perks", (int)InvenKeys.e_perk));
+        InvenRef.Add(new KeyValuePair<string, int>("Ships", (int)InvenKeys.e_ship));
+        InvenRef.Add(new KeyValuePair<string, int>("Guns", (int)InvenKeys.e_gun));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
