e0d523855593fefa56acddf625503be8de47f7a8
rexov <jack.howard9898@gmail.com>
2018-04-26 10:40:00 +0100

    return to action menu after attacks and defences

--- jrpg/Assets/Scripts/UI/battleUIs/attackSelection.cs
+++ jrpg/Assets/Scripts/UI/battleUIs/attackSelection.cs
@@ -26,8 +26,10 @@ public class attackSelection : MonoBehaviour {
     public string currentAction = "null";
 
     public GameObject targets;//the player or enemy fleet
+
     private bool active = true;
     public BattleManager bm;
+
 
     // Use this for initialization
     void Start ()
@@ -45,60 +47,55 @@ public class attackSelection : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (active) { 
-        if (Input.GetKeyDown(KeyCode.W))
-            currentSelection--;
+        if (active) {
+            if (Input.GetKeyDown(KeyCode.W))
+                currentSelection--;
 
-        if (Input.GetKeyDown(KeyCode.S))
-            currentSelection++;
+            if (Input.GetKeyDown(KeyCode.S))
+                currentSelection++;
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            actionMenu.SetActive(true);
-            currentMenu.SetActive(false);
-        }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                actionMenu.SetActive(true);
+                currentMenu.SetActive(false);
+            }
 
-        if (currentSelection > numButtons)//loops the selection
-            currentSelection = 1;
-        else if (currentSelection < 1)
-            currentSelection = numButtons;
+            if (currentSelection > numButtons)//loops the selection
+                currentSelection = 1;
+            else if (currentSelection < 1)
+                currentSelection = numButtons;
 
-        if (currentSelection == 1)
-        {
-            
-            renderer1.color = selectColour;
-            renderer2.color = defaultColour;
-            renderer4.color = defaultColour;
-        }
-        else if (currentSelection == 2)
-        {
-            renderer2.color = selectColour;
-            renderer1.color = defaultColour;
-            renderer3.color = defaultColour;
-        }
-        else if (currentSelection == 3)
-        {
-            renderer3.color = selectColour;
-            renderer2.color = defaultColour;
-            renderer4.color = defaultColour;
-        }
-        else if (currentSelection == 4)
-        {
-            renderer4.color = selectColour;
-            renderer1.color = defaultColour;
-            renderer3.color = defaultColour;
-        }
+            if (currentSelection == 1)
+            {
+
+                renderer1.color = selectColour;
+                renderer2.color = defaultColour;
+                renderer4.color = defaultColour;
+            }
+            else if (currentSelection == 2)
+            {
+                renderer2.color = selectColour;
+                renderer1.color = defaultColour;
+                renderer3.color = defaultColour;
+            }
+            else if (currentSelection == 3)
+            {
+                renderer3.color = selectColour;
+                renderer2.color = defaultColour;
+                renderer4.color = defaultColour;
+            }
+            else if (currentSelection == 4)
+            {
+                renderer4.color = selectColour;
+                renderer1.color = defaultColour;
+                renderer3.color = defaultColour;
+            }
 
             if (Input.GetKeyDown(KeyCode.E))
             {
                 if (currentSelection == 1)//the player selected the first action
                 {
                     currentAction = "AttackSelect1";//this needs to tell the gamemanager what attack to do
-                    //Debug.Log("attack chosen");
-                    //var newTurn = GameObject.Find("TurnHolder").AddComponent<TurnAction>();
-                    //set the new ship
-                    //set the 
-
                 }
                 else if (currentSelection == 2)
                 {
@@ -121,6 +118,14 @@ public class attackSelection : MonoBehaviour {
         {
             targets.SetActive(false);
             active = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && active == false)
+        {
+            active = true;
+            targets.SetActive(false);
+            currentMenu.SetActive(false);
+            actionMenu.SetActive(true);
+
         }
     }
 }
